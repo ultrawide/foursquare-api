a2f4595faf38f1ae73d07fd5312e30e433bdf138
Colin Chan <colinchan02@gmail.com>
2021-01-02 01:08:20 -0800

    Install dotenv dependency

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const request = require('request');
 
 request(
@@ -5,8 +6,8 @@ request(
     url: 'https://api.foursquare.com/v2/venues/explore',
     method: 'GET',
     qs: {
-      client_id: 'CLIENT_ID',
-      client_secret: 'CLIENT_SECRET',
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
       ll: '40.7243,-74.0018',
       query: 'coffee',
       v: '20180323',
